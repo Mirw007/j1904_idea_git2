99047408ceb21e44a47b10f2931416dd041d5588
Mirw007 <1505174595@qq.com>
2019-08-22 19:28:09 +0800

    abc

--- src/main/java/com/qf/j1904/pojo/Dept.java
+++ src/main/java/com/qf/j1904/pojo/Dept.java
@@ -1,11 +1,12 @@
 package com.qf.j1904.pojo;
 
 /**
- *add .(将文件系统的workspace 提交给本地maven 的缓存)
+ * add .(将文件系统的workspace 提交给本地maven 的缓存)
  * commit
+ *
  */
 public class Dept {
 
-    private int deptno;
+    private int deptno;//这是先到缓存
     private String dname;
 }
